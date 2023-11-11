cb2507d4404298eb671ed1d4f1386ea5d30db71f
Nga Hoang V <ngahv2222@gmail.com>
2023-11-11 23:24:02 +0700

    Change deploy by assume role in github action

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Style from './Home.module.scss';
-import me from '../../img/self.png';
 import classNames from 'classnames';
 import EmojiBullet from "./EmojiBullet";
 import SocialIcon from "./SocialIcon";
@@ -13,9 +12,18 @@ export default function Home(props) {
    return (
       <Box component={'main'} display={'flex'} flexDirection={{ xs: 'column', md: 'row' }} alignItems={'center'}
          justifyContent={'center'} minHeight={'calc(100vh - 175px)'}>
-         <Box className={classNames(Style.avatar, Style.shadowed)} alt={'image of developer'} style={{ background: info.gradient }} component={'img'} src={me} width={{ xs: '35vh', md: '40vh' }}
+         <Box
+            className={classNames(Style.avatar, Style.shadowed)}
+            alt={'image of developer'}
+            style={{ background: info.gradient }}
+            component={'img'}
+            src={authorInfo?.avatar}
+            width={{ xs: '35vh', md: '40vh' }}
             height={{ xs: '35vh', md: '40vh' }}
-            borderRadius={'50%'} p={'0.75rem'} mb={{ xs: '1rem', sm: 0 }} mr={{ xs: 0, md: '2rem' }} />
+            borderRadius={'50%'} p={'0.75rem'}
+            mb={{ xs: '1rem', sm: 0 }}
+            mr={{ xs: 0, md: '2rem' }} 
+         />
          {
             bio ?
                <Box>
